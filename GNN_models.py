5aac5d4465fd6466edc166658de1226f1d751394
ivan <ivan.ploxix@gmail.com>
2023-07-11 18:06:50 +0700

    Removed skipcons

--- GNN_models.py
+++ GNN_models.py
@@ -97,7 +97,7 @@ class FlowGNN_original_skipBC(nn.Module):  # напрямую прокидыва
 
 class FlowGNN_conv_block(nn.Module):
 
-    def __init__(self, edge_dims, node_dims, hidden_size=128, node_skip_cons_idx=None, edge_skip_cons_idx=None,
+    def __init__(self, edge_dims, node_dims, hidden_size=128,
                  fc_con=False, batchnorm=False, idx=0, selu=False):
         super(FlowGNN_conv_block, self).__init__()
         self.conv = ProcessorLayer(edge_dims, node_dims, hidden_size, selu)
@@ -107,19 +107,11 @@ class FlowGNN_conv_block(nn.Module):
         if batchnorm:
             self.node_norm_layer = nn.BatchNorm1d(node_dims)
 
-        self.node_skip_cons_idx = node_skip_cons_idx  # int, layer indx
-        self.edge_skip_cons_idx = edge_skip_cons_idx
         self.fc_con = fc_con  # bool,
         self.idx = idx
 
-    def forward(self, node_attr, edge_idx, edge_attr, node_skip_cons=None, edge_skip_cons=None, fc_con=None,
+    def forward(self, node_attr, edge_idx, edge_attr, fc_con=None,
                 skip_info=None):
-
-        if node_skip_cons is not None:
-            node_attr = torch.add(node_attr, node_skip_cons)
-
-        if edge_skip_cons is not None:
-            edge_attr = torch.add(edge_attr, edge_skip_cons)
 
         if skip_info is not None:
             node_attr = torch.cat([node_attr, skip_info], 1)
@@ -162,7 +154,6 @@ class FlowGNN_fc_block(nn.Module):
 class FlowGNN(nn.Module):  # универсальная модель
 
     def __init__(self, edge_filters, node_filters, fc_in_dim, fc_out_dim,
-                 node_skip_cons_list=None, edge_skip_cons_list=None,
                  fc_con_list=None, fc_hidden_layers=(128, 128),
                  batchnorm=None, selu=None,
                  geom_in_dim=2, out_dim=4):
@@ -172,11 +163,6 @@ class FlowGNN(nn.Module):  # универсальная модель
         self.node_filters = node_filters
         self.fc_in_dim = fc_in_dim
         self.fc_out_dim = fc_out_dim
-
-        # каждому слою сопоставляется множество слоев из которых приходят SC
-        # [None, None, None, [0,1,2],...] В блок № 3 прокинуты скип коннекшены из блоков №№0-2
-        self.node_skip_cons_list = node_skip_cons_list
-        self.edge_skip_cons_list = edge_skip_cons_list
 
         self.fc_con_list = fc_con_list  # [1,3,4,6] слои куда заходит fc слой
         self.fc_hidden_layers = fc_hidden_layers
@@ -196,8 +182,6 @@ class FlowGNN(nn.Module):  # универсальная модель
                 fc_con = True
                 self.fc_layers_list.append(FlowGNN_fc_block(self.fc_out_dim, self.fc_hidden_layers))
             self.gcnn_layers_list.append(FlowGNN_conv_block(ef, nf,
-                                                            node_skip_cons_idx=self.node_skip_cons_list[i],
-                                                            edge_skip_cons_idx=self.edge_skip_cons_list[i],
                                                             fc_con=fc_con,
                                                             batchnorm=self.batchnorm[i], idx=i,
                                                             selu=self.selu[i]))
@@ -209,8 +193,6 @@ class FlowGNN(nn.Module):  # универсальная модель
 
         x_outs = {}  # nodes
         edge_outs = {}  # edges
-        skip_con_nodes = None
-        skip_con_edges = None
         skip_info = x[:, :self.geom_in_dim]
         fc_out = None
         if self.fc_con_list is not None:
@@ -219,25 +201,13 @@ class FlowGNN(nn.Module):  # универсальная модель
 
         for i, layer in enumerate(self.gcnn_layers_list):
 
-            if layer.node_skip_cons_idx is not None:
-                skip_con_nodes = x_outs[layer.node_skip_cons_idx[0]]
-                for i in layer.node_skip_cons_idx[1:]:
-                    skip_con_nodes += x_outs[i]
-
-            if layer.edge_skip_cons_idx is not None:
-                skip_con_edges = edge_outs[layer.edge_skip_cons_idx[0]]
-                for i in layer.edge_skip_cons_idx[1:]:
-                    skip_con_edges += edge_outs[i]
-
             if layer.idx in self.fc_con_list[1:]:
                 graph_pool = global_mean_pool(x, data.batch)
                 graph_pool = graph_pool[data.batch]
                 fc_out = self.fc_layers_list[fc_count](torch.cat([data.bc, fc_out, graph_pool], 1))
                 fc_count += 1
-            x, edge_attr = layer(x, data.edge_index, data.edge_attr, skip_con_nodes, skip_con_edges, fc_out, skip_info)
+            x, edge_attr = layer(x, data.edge_index, data.edge_attr, fc_out, skip_info)
 
-            skip_con_nodes = None
-            skip_con_edges = None
             x_outs[i] = x
             edge_outs[i] = edge_attr
 
